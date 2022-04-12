d6e1fc638fb2fd7eaf9c442af26430fc26b95a8f
Adnane Lamghari <adnane.lamghari@rutabago.com>
2022-04-13 01:26:59 +0200

    update krat id

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 // in a real life project we can use an .env file
 exports.END_POINT = 'https://recrutement-practice-default-rtdb.firebaseio.com/';
 
-exports.KRAT_ID = '5bb62f1f19dde9b8d7aa';
+exports.KRAT_ID = 'a698f14af964b83b9c84';
 exports.KRAT_URL = 'https://krat.es';
